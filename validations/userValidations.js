393f25b785f4732b4d140ea158171b1f471498a8
Kevin04C <kevincespedes2004@gmail.com>
2023-03-05 22:58:56 -0500

    Add userValidantions

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -1,10 +1,25 @@
-import { request, response } from 'express'
-import { body } from 'express-validator'
+import { body, query } from 'express-validator'
 import { fieldValidations } from '../utils/fieldValidations.js'
 
-export const validationRegisterUser = [
+export const validationRegister = [
   body('username').not().isEmpty(),
   body('email').not().isEmpty().isEmail().normalizeEmail(),
   body('password').not().isEmpty().isLength({ min: 6 }),
+  fieldValidations
+]
+
+export const validationLogin = [
+  body('username').not().isEmpty(),
+  body('password').not().isEmpty().isLength({ min: 6 }),
+  fieldValidations
+]
+
+export const validationAutWithGoogle = [
+  body('credential').not().isEmpty(),
+  fieldValidations
+]
+
+export const validationVerifyAccount = [
+  query('token').not().isEmpty(),
   fieldValidations
 ]
