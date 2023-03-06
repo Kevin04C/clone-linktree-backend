247320969f4ff52a90da3d50827e8cd6ece6e5e4
Kevin04C <kevincespedes2004@gmail.com>
2023-03-05 22:59:06 -0500

    add verifyAccount

---
+++ middlewares/verifiedAccount.js
@@ -0,0 +1,22 @@
+import { request, response } from 'express'
+import { User } from '../models/User.js'
+import { sendEmail } from '../services/sendEmail.js'
+import { handleErrorResponse } from '../utils/handleErrors.js'
+
+export const verifiedAccount = async (req = request, res = response, next) => {
+  const { username } = req.body
+  try {
+    const user = await User.findUsername(username)
+    if (user.verify === 0) {
+      sendEmail(user)
+      return res.status(401).json({
+        ok: false,
+        msg: 'account is not verify, we have send an email to verify'
+      })
+    }
+    req.user = user
+  } catch (error) {
+    handleErrorResponse()
+  }
+  next()
+}
